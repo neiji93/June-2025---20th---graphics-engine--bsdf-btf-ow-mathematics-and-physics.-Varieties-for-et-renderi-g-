ca38a1155eb6fa6ebf09072f97d12e9e464ad4bd
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-06-22 07:31:44 +0200

    Create TransmissionLowLevel.hlsl

---
+++ Bsdf/TransmissionLowLevel.hlsl
@@ -0,0 +1,6 @@
+//especizlly made for renderi'g equation kajiya.
+//then, use a float number at final stage of integrated to
+//clamp samples. it means we can use shadow.mzps too.
+//but a more elegant way that we could do here will be
+//to use brandtl model generated high term.equation.
+//then, to be elegant, lets use shadow frame.
