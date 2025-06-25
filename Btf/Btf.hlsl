2a353d7f50bf0767d9f5c5d390f15eb049345c23
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-06-25 06:05:44 +0200

    Update Btf.hlsl

--- Btf/Btf.hlsl
+++ Btf/Btf.hlsl
@@ -1,1 +1,5 @@
 //image sensors can start with right bottom corner image.
+
+
+It means phsyically that WE Can use pro p groups .
+Void nonciphernonhilbertilbertspacehighalgebraicvaluesbyexperimentalmathmaticqfortra smissionvalues
